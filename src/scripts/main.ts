038bbf8a59d81fe359ea98a5947452f65580c6f1
anmol-thedev <anmolsingh79102@gmail.com>
2025-08-01 12:11:37 +0530

    few changes in description

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -4,6 +4,7 @@ const mq = window.matchMedia("(min-width: 640px)");
 if (mq.matches) {
 	contextCursor({
 		radius: 25,
+		transitionSpeed: 0.2,
 	});
 	
 	const container = document.documentElement;
